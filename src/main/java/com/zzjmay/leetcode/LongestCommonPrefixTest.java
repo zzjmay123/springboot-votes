1bfd945118bae9b5e9308af126ea80e28b728f7e
zhouzhenjiang3 <zhouzhenjiang@jd.com>
2019-02-23 15:31:18 +0800

    算法题目

---
+++ src/main/java/com/zzjmay/leetcode/LongestCommonPrefixTest.java
@@ -0,0 +1,32 @@
+package com.zzjmay.leetcode;
+
+/**
+ * LeetCode - 最长公共前缀
+ * Created by zzjmay on 2019/2/22.
+ */
+public class LongestCommonPrefixTest {
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","fight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    private static String longestCommonPrefix(String[] strs){
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        int len = strs.length;
+        for (int i = 1;i< len;i++){
+
+            //两两比较
+            while(!strs[i].startsWith(prefix)){
+               //如果没有匹配的前醉，则减去一个
+                prefix = prefix.substring(0,prefix.length()-1);
+            }
+            if("".equals(prefix)){
+                break;
+            }
+        }
+
+        return prefix;
+    }
+}
---
+++ src/main/java/com/zzjmay/leetcode/SubStringTest.java
@@ -0,0 +1,72 @@
+package com.zzjmay.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by zzjmay on 2019/2/22.
+ */
+public class SubStringTest {
+
+    public static void main(String[] args) {
+
+        String test = "wdkwsdofdxvyzlg";
+
+        System.out.println(lengthOfLongestSubstring2(test));
+    }
+
+    /**
+     * 无重复字符的最长子串
+     * 首先你得是子串，不单单是去重
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+
+        //1.初始化一个map，用于存放无重复字符及其位置
+        Map<Character, Integer> map = new HashMap<>();
+        //最长长度
+        int maxLength = 0;
+        //初始的指针
+        int now = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            //2.判断当前字符是否存在
+            if (map.containsKey(s.charAt(i))) {
+                //如果存在在map中，则头部指针，指针指向当前位置和当前重复元素下一个位置的最大值
+                now = Math.max(map.get(s.charAt(i))+1,now);
+                if((i-now+1) > maxLength){
+                    maxLength = i-now+1;
+                }
+            } else {
+                //不存在，就计算当前位置和初始指针的长度，是否大于最长长度
+                maxLength = Math.max(maxLength,i-now+1);
+            }
+
+            map.put(s.charAt(i), i);
+        }
+
+
+        return maxLength;
+    }
+
+    public static int lengthOfLongestSubstring2(String s){
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}

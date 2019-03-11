bbe7d35e77fc96d502665266f81ab7ad7f33f938
zhouzhenjiang3 <zhouzhenjiang@jd.com>
2019-03-11 16:46:59 +0800

    解决刚刚系统设置时间的问题

--- src/main/java/com/zzjmay/completableFuture/Test.java
+++ src/main/java/com/zzjmay/completableFuture/Test.java
@@ -1,28 +1,29 @@
 package com.zzjmay.completableFuture;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by zzjmay on 2019/3/11.
  */
 public class Test {
 
-    @org.junit.Test
-    public void schedule(){
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    public static void main(String[] args) throws Exception {
 
-        System.out.println("3s后开始执行线程池服务");
-        final long start = System.currentTimeMillis();
-        scheduledExecutorService.schedule(new Runnable() {
-            @Override
-            public void run() {
+        ExecutorService executorService = Executors.newFixedThreadPool(30);
 
-                System.out.println("*********");
-                System.out.println("耗时 times："+(System.currentTimeMillis() - start));
-
-            }
-        }, 15, TimeUnit.SECONDS);
+        Future<String> future = executorService.submit(()->{
+           System.out.println("执行任务111******");
+           Thread.sleep(5000);
+           System.out.println("执行任务结束222****");
+           return "hello world";
+        });
+        try {
+            String s = future.get(100, TimeUnit.MILLISECONDS);
+        }catch (TimeoutException ex){
+            future.cancel(true);
+            System.out.println("执行了任务的删除，防止占用线程");
+        }finally {
+//            executorService.shutdown();
+        }
     }
 }
